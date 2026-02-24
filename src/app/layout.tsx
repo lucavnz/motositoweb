593fa095cccbd48877d1eee8260cac9e87d6c8b4
Luca Avanzi <lucaavanzi62@gmail.com>
2026-02-24 19:45:17 +0100

    asdsa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,6 +56,10 @@ export async function generateMetadata(): Promise<Metadata> {
       index: true,
       follow: true,
     },
+    icons: {
+      icon: '/AvanzimotoLOGO.webp',
+      apple: '/AvanzimotoLOGO.webp',
+    },
   }
 }
 
